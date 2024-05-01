19f88b2edd5906b105511198c1b4e9613d991277
Thinh Nguyen <gaupoit@outlook.com>
2024-05-01 11:28:34 +0700

    Display play now btn

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,36 @@
-import { Providers } from './providers';
-import { AppProps } from 'next/app';
-import localFont from 'next/font/local';
-import './globals.css';
+import { AppProps } from "next/app";
+import { WagmiProvider } from "wagmi";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import localFont from "next/font/local";
+import { http, createConfig } from "wagmi";
+import { base, mainnet, optimism } from "wagmi/chains";
+import { injected, metaMask, safe, walletConnect } from "wagmi/connectors";
 
-const myFont = localFont({ src: './SuperMario256.ttf', variable: '--my-font' });
+import "./globals.css";
+
+const myFont = localFont({ src: "./SuperMario256.ttf", variable: "--my-font" });
+
+const projectId = "bc74d9ca10f837f40de70bddb477484a";
+export const wagmiConfig = createConfig({
+  chains: [mainnet, base],
+  connectors: [walletConnect({ projectId })],
+  transports: {
+    [mainnet.id]: http(),
+    [base.id]: http(),
+  },
+});
+
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Providers>
-      <main className={`${myFont.variable} !font-sans`}>
-        <Component {...pageProps} />
-      </main>
-    </Providers>
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <main className={`${myFont.variable} !font-sans`}>
+          <Component {...pageProps} />
+        </main>
+      </QueryClientProvider>
+    </WagmiProvider>
   );
 }
 
--- pages/home/main-card/main-card.tsx
+++ pages/home/main-card/main-card.tsx
@@ -1,16 +1,5 @@
-import { Button, Card, Typography, notification } from 'antd';
-import { signMessage } from '@casperdash/usewallet-core';
-import { useGetLinkedAccount } from '@/modules/hooks/useGetLinkedAccount';
-import { MiddleTruncatedText } from '@/modules/components/middle-truncated-text';
-import { UserAssets } from './user-assets';
-import { useLinkAccount } from '@/modules/hooks/useLinkAccount';
-import { useGetCDToken } from '@/modules/hooks/useGetAssets';
-import {
-  useAccount,
-  useDisconnect,
-  CasperDashConnector,
-  useConnect,
-} from '@casperdash/usewallet';
+import { Button, Card, Typography, notification } from "antd";
+import { useAccount } from "wagmi";
 
 interface MainCardProps {
   userData: any;
@@ -18,84 +7,21 @@ interface MainCardProps {
 }
 
 export const MainCard = ({ userData = {} }: MainCardProps) => {
-  const { data, refetch } = useGetLinkedAccount(userData.sub);
-  const { data: CDToken } = useGetCDToken(userData.sub);
-  const { publicKey } = useAccount();
-  const { disconnect } = useDisconnect();
-
-  const { connect: connectWithCasperDash } = useConnect({
-    connector: new CasperDashConnector(),
-  });
-
-  const { mutate: link } = useLinkAccount();
-  const onLink = async () => {
-    const singedMessage = await signMessage({
-      message: `Linking ${userData.sub} with ${publicKey}`,
-      signingPublicKeyHex: publicKey!,
-    });
-    if (singedMessage) {
-      link(
-        {
-          userId: userData.sub,
-          publicKey: publicKey!,
-          signedMessage: singedMessage,
-        },
-        {
-          onSuccess: () => refetch(),
-          onError: (error) => {
-            notification.error({
-              message: 'Your wallet is already linked to another account!',
-            });
-          },
-        },
-      );
-    }
-  };
+  const { isConnected, address } = useAccount();
   const onPlay = () => {
-    window.open('https://www.roblox.com/games/15699428870/BETA');
+    window.open("https://www.roblox.com/games/15699428870/BETA");
   };
 
   return (
     <Card className="w-3/4 opacity-90 ">
       <div className="flex gap-4">
         <Typography.Title level={3}>Account Info</Typography.Title>
-        {data?.publicKey && (
+        {isConnected && (
           <Button type="primary" onClick={onPlay}>
             Play Now !
           </Button>
         )}
       </div>
-      <div className="flex gap-4 items-center">
-        <Typography.Text className="font-semibold !text-gray-500">
-          Linked Wallet:
-        </Typography.Text>
-        {data?.publicKey ? (
-          <div className="flex items-center gap-2">
-            <MiddleTruncatedText text={data.publicKey} />
-          </div>
-        ) : (
-          <>
-            {!publicKey ? (
-              <Button type="primary" onClick={() => connectWithCasperDash()}>
-                Connect Wallet
-              </Button>
-            ) : (
-              <Button type="primary" onClick={() => onLink()}>
-                Link Wallet
-              </Button>
-            )}
-          </>
-        )}
-      </div>
-      <div className="flex gap-4 items-center">
-        <Typography.Text className="font-semibold">
-          <span className="text-gray-500">CD Token:</span> {CDToken?.total}
-        </Typography.Text>
-      </div>
-      <Typography.Title level={4} className="mt-4">
-        Assets
-      </Typography.Title>
-      <UserAssets userId={userData.sub} />
     </Card>
   );
 };
